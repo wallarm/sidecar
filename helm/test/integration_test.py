41baf2d51ea2de3f797f870bfdd6da87573176b1
Evgeniy Dmitriev <edmitriev@wallarm.com>
2022-07-25 14:35:12 +0700

    Refactor integration tests. Moved its execution into k8s Pod

--- helm/test/integration_test.py
+++ helm/test/integration_test.py
@@ -25,44 +25,45 @@ class Helpers:
     def subprocess_run(cmd: str) -> subprocess.CompletedProcess:
         logger.info(f'Command: {cmd}')
         completed_process = subprocess.run(shlex.split(cmd), capture_output=True, text=True)
-        logger.info(f'Finished with exit code: {completed_process.returncode}')
+        if completed_process.returncode != 0:
+            logger.error(completed_process.stderr)
+            raise Exception(f'Command: {cmd} '
+                            f'Exit code: {completed_process.returncode} '
+                            f'Stderr: {completed_process.stderr}')
         return completed_process
 
     @staticmethod
-    def create_namespace(namespace: str, context: str) -> subprocess.CompletedProcess:
-        cmd = f'kubectl --context {context} create namespace {namespace}'
+    def create_namespace(namespace: str) -> None:
+        cmd = f'kubectl create namespace {namespace}'
         logger.info('Create namespace ...')
-        return Helpers.subprocess_run(cmd)
+        Helpers.subprocess_run(cmd)
 
     @staticmethod
-    def create_resources(path: str, namespace: str, context: str) -> subprocess.CompletedProcess:
-        cmd = f'kubectl --context {context} --namespace {namespace} create -k {path}/'
+    def create_resources(path: str, namespace: str) -> None:
+        cmd = f'kubectl --namespace {namespace} create -k {path}/'
         logger.info('Create resources ...')
-        return Helpers.subprocess_run(cmd)
+        Helpers.subprocess_run(cmd)
 
     @staticmethod
-    def wait_pods(namespace: str, context: str) -> subprocess.CompletedProcess:
+    def wait_pods(namespace: str) -> None:
         # Need delay here because in some cases we have error: no matching resources found
         sleep(2)
-        cmd = f'kubectl --context {context} --namespace {namespace} wait --for=condition=Ready pods --all --timeout={WAIT_PODS_TIMEOUT}'
+        cmd = f'kubectl --namespace {namespace} ' \
+              f'wait --for=condition=Ready pods --all --timeout={WAIT_PODS_TIMEOUT}'
         logger.info('Wait for all Pods ready ...')
-        return Helpers.subprocess_run(cmd)
+        Helpers.subprocess_run(cmd)
 
     @staticmethod
-    def delete_namespace(namespace: str, context: str) -> subprocess.CompletedProcess:
-        cmd = f'kubectl --context {context} delete namespace {namespace}'
+    def delete_namespace(namespace: str) -> None:
+        cmd = f'kubectl delete namespace {namespace}'
         logger.info('Delete namespace ...')
-        return Helpers.subprocess_run(cmd)
+        Helpers.subprocess_run(cmd)
 
     @staticmethod
-    def setup_resources(path: str, namespace: str, context: str) -> subprocess.CompletedProcess:
-        create_namespace = Helpers.create_namespace(namespace, context)
-        if create_namespace.returncode != 0:
-            return create_namespace
-        create_resources = Helpers.create_resources(path, namespace, context)
-        if create_resources.returncode != 0:
-            return create_resources
-        return Helpers.wait_pods(namespace, context)
+    def setup_resources(path: str, namespace: str) -> None:
+        Helpers.create_namespace(namespace)
+        Helpers.create_resources(path, namespace)
+        Helpers.wait_pods(namespace)
 
 
 @pytest.fixture(scope="function")
@@ -70,40 +71,27 @@ def helpers():
     return Helpers
 
 
-@pytest.fixture(scope="session")
-def pytest_config(pytestconfig):
-    base_url = f'http://{pytestconfig.getoption("host")}:{pytestconfig.getoption("port")}'
-    return dict(
-        allowed_url=base_url + ALLOWED_HTTP_PATH,
-        forbidden_url=base_url + FORBIDDEN_HTTP_PATH,
-        kube_context=pytestconfig.getoption("kube_context")
-    )
-
-
 @pytest.fixture(scope="function")
 def teardown_namespace():
     config = {}
     yield config
-    logger.info('Teardown ...')
-    Helpers.delete_namespace(config['namespace'], config['kube_context'])
+    namespace = config['namespace']
+    logger.info(f'Teardown namespace {namespace} ...')
+    Helpers.delete_namespace(namespace)
 
 
 class TestMainFunctionality:
     @pytest.mark.parametrize("config", patchList)
-    def test_main_functionality(self, config, helpers, pytest_config, teardown_namespace):
+    def test_main_functionality(self, config, helpers, teardown_namespace):
         config_path = f'{PATCHES_PATH}/{config}'
         namespace = config.replace('_', '-')
+        base_url = f'http://dummy-app-svc.{namespace}.svc'
+        allowed_url = base_url + ALLOWED_HTTP_PATH
+        forbidden_url = base_url + FORBIDDEN_HTTP_PATH
 
-        allowed_url = pytest_config['allowed_url']
-        forbidden_url = pytest_config['forbidden_url']
-        kube_context = pytest_config['kube_context']
-
-        # Register teardown
+        # Register teardown and setup resources for test
         teardown_namespace['namespace'] = namespace
-        teardown_namespace['kube_context'] = kube_context
-
-        setup_resources = helpers.setup_resources(config_path, namespace, kube_context)
-        assert setup_resources.returncode == 0, setup_resources.stderr
+        helpers.setup_resources(config_path, namespace)
 
         # Need delay here to ensure that service is ready to send traffic to pods
         sleep(2)
